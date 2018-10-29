b28c1d65f6fd96f5b89cc0de963ace919edf34e3
pedditarakaraviteja <pedditarakaraviteja@gmail.com>
2018-10-28 21:05:43 -0500

    up2

--- test/a03test.js
+++ test/a03test.js
@@ -15,13 +15,7 @@
 
 // The following is a helpful summary of all the QUnit asserts: 
 
-//deepEqual(value, expected[, message]): A recursive, strict comparison that works on all the JavaScript types. The assertion passes if value and expected are identical in terms of properties, values, and they have the same prototype;
-//equal(value, expected[, message]): Verify the value provided is equal the expected parameter using a non-strict comparison (==).
-//notDeepEqual(value, expected[, message]): Same as deepEqual() but tests for inequality;
-//notEqual(value, expected[, message]): Same as equal() but tests for inequality;
-//propEqual(value, expected[, message]): A strict comparison of the properties and values of an object. The assertion passes if all the properties and the values are identical;
-//strictEqual(value, expected[, message]): Verify the value provided is equal to the expected parameter using a strict comparison (===);
-//notPropEqual(value, expected[, message]): Same as propEqual() but tests for inequality;
+
 //notStrictEqual(value, expected[, message]): Same as strictEqual() but tests for inequality;
 //ok(value[, message]: An assertion that passes if the first argument is truthy;
 //throws(function [, expected ] [, message ]): Test if a callback throws an exception, and optionally compare the thrown error;
@@ -36,6 +30,6 @@ QUnit.test('Testing calculate square,reverse,strong number', function (assert) {
     assert.throws(function () { square(); }, /The given argument is not a number/, 'Passing in null correctly raises an Error');
         //throws( block                                    [, expected ] [, message ] ) 
   assert.throws(function () { reverse(); }, /The given argument is not a number/, 'Passing in null correctly raises an Error');
-   // assert.throws(function () { vfirst("a.@#"); }, /The given argument is not a valid name/, 'Passing in a string with special characters raises an Error');
+   
 
 });
